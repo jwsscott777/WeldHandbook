42422a2a94025a3d2ab3fcb69efb53215b991c8e
JWSScott777 <jwsscott777@gmail.com>
2023-12-08 21:43:38 -0500

    Another change

---
+++ WeldBook Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeldBook Watch App
+//
+//  Created by JWSScott777 on 12/8/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("It is weird")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeldBook Watch App/WeldBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeldBookApp.swift
+//  WeldBook Watch App
+//
+//  Created by JWSScott777 on 12/8/23.
+//
+
+import SwiftUI
+
+@main
+struct WeldBook_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
