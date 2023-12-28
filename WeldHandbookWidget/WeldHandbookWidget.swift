e73b4dcd1a81331b8749c3ec2c3068e75bb32ae4
JWSScott777 <jwsscott777@gmail.com>
2023-12-28 17:34:09 -0500

    Added Widget

---
+++ WeldHandbookWidget/WeldHandbookWidget.swift
@@ -0,0 +1,140 @@
+//
+//  WeldHandbookWidget.swift
+//  WeldHandbookWidget
+//
+//  Created by JWSScott777 on 12/28/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+struct Provider: TimelineProvider {
+  //  var list: ListModel = lists[3]
+    func placeholder(in context: Context) -> SimpleEntry {
+        SimpleEntry(date: Date(), listModel: lists[0])
+    }
+
+    func getSnapshot(in context: Context, completion: @escaping (SimpleEntry) -> ()) {
+        let entry = SimpleEntry(date: Date(), listModel: lists.randomElement() ?? lists[0])
+        completion(entry)
+    }
+
+    func getTimeline(in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
+   //     var entries: [SimpleEntry] = []
+
+//        for hourOffset in 0 ..< 5 {
+//            let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
+//            let entry = SimpleEntry(date: entryDate)
+//            entries.append(entry)
+//        }
+        let currentDate = Date()
+        guard let refreshDate = Calendar.current.date(byAdding: .day, value: 1, to: currentDate) else { return  }
+        let entry = SimpleEntry(date: currentDate, listModel: lists.randomElement() ?? lists[0])
+
+        let timeline = Timeline(entries: [entry], policy: .after(refreshDate))
+        completion(timeline)
+    }
+}
+
+struct SimpleEntry: TimelineEntry {
+    let date: Date
+    let listModel: ListModel
+
+}
+
+struct WeldHandbookWidgetEntryView : View {
+    var entry: Provider.Entry
+    @Environment(\.widgetFamily) var widgetFamily
+
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                backgroundGradient
+                Image("WeldBook")
+                    .resizable()
+                    .frame(
+                        width: widgetFamily != .systemSmall ? 56 : 36,
+                        height: widgetFamily != .systemSmall ? 56 : 36
+                    )
+                    .clipShape(RoundedRectangle(cornerRadius: 16))
+                    .offset(x: (geometry.size.width / 2) - 20, y: (geometry.size.height / -2) + 20)
+                    .padding(.top, 12)
+                    .padding(.trailing, 12)
+                HStack {
+                    Text("Weld Word Daily")
+                        .foregroundStyle(.white)
+                        .font(widgetFamily != .systemSmall ? .system(.footnote, design: .rounded) : .system(size: 08))
+                        .fontWeight(.bold)
+                        .multilineTextAlignment(.center)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 4)
+                        .background(
+                            Color(red: 0, green: 0, blue: 0, opacity: 0.5)
+                                .blendMode(.overlay)
+                        )
+                    .clipShape(Capsule())
+                }// hs
+
+                .offset(y: (geometry.size.height /  2) - 24)
+                HStack() {
+                    Image(systemName: entry.listModel.image)
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(.darkBlue)
+                        .frame(
+                            width: widgetFamily != .systemSmall ? 56 : 36,
+                            height: widgetFamily != .systemSmall ? 56 : 36
+                        )
+                    Text(entry.listModel.title)
+                        .foregroundStyle(.white)
+                        .fontWeight(.bold)
+                        .font(widgetFamily != .systemSmall ? .title2 : .system(size: 09))
+                        .multilineTextAlignment(.center)
+                } // vs
+                .padding(.bottom)
+                .padding(.top)
+            } // zs
+        } // geo
+    }
+}
+
+struct WeldHandbookWidget: Widget {
+    let kind: String = "WeldHandbookWidget"
+
+    var body: some WidgetConfiguration {
+        StaticConfiguration(kind: kind, provider: Provider()) { entry in
+            if #available(iOS 17.0, *) {
+                WeldHandbookWidgetEntryView(entry: entry)
+                    .containerBackground(for: .widget) {
+                        Color.purple
+                    }
+            } else {
+                WeldHandbookWidgetEntryView(entry: entry)
+                    .padding()
+                    .background()
+            }
+        }
+        .configurationDisplayName("Welding Handbook")
+        .description("Learn a new word about welding every day!.")
+    }
+}
+
+#Preview(as: .systemSmall) {
+    WeldHandbookWidget()
+} timeline: {
+    SimpleEntry(date: .now, listModel: lists[33])
+}
+#Preview(as: .systemMedium) {
+    WeldHandbookWidget()
+} timeline: {
+    SimpleEntry(date: .now, listModel: lists[33])
+}
+#Preview(as: .systemLarge) {
+    WeldHandbookWidget()
+} timeline: {
+    SimpleEntry(date: .now, listModel: lists[33])
+}
+
+var backgroundGradient: LinearGradient {
+    return LinearGradient(gradient: Gradient(colors: [Color.indigo, Color.pink]), startPoint: .topLeading, endPoint: .bottomTrailing)
+}
---
+++ WeldHandbookWidget/WeldHandbookWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WeldHandbookWidgetBundle.swift
+//  WeldHandbookWidget
+//
+//  Created by JWSScott777 on 12/28/23.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WeldHandbookWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        WeldHandbookWidget()
+    }
+}
